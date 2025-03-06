73eee4cec436b862cfcc3fd1261eb58312ecbfde
Maaz Ali <abrahamlincoln1027@gmail.com>
2025-03-06 23:48:38 +0500

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const user = {
-    name: "Maaz Ali",
+    name: "User",
     profilePic: "https://via.placeholder.com/40"
   };
 
